d9d86182568f819c604c319dbe07e90f686777c2
avilavate <avilavate123@gmail.com>
2017-06-27 12:51:09 +0530

    Prepared before (oversubscription) scenario.

--- Load_Balanced_by_Cores/before.cs
+++ Load_Balanced_by_Cores/before.cs
@@ -11,14 +11,26 @@ namespace Load_Balanced_by_Cores
     {
       public void OverSubscription()
         {
-            List<Task> tasks = new List<Task>();
+            List<Task<string>> tasks = new List<Task<string>>();
             for (int i = 0; i < constants.N; i++)
             {
-                tasks.Add(Task.Factory.StartNew(() =>
+                tasks.Add(Task.Factory.StartNew<string>(() =>
                 {
-                    Thread.Sleep(constants.Delay);
-                    Console.WriteLine("Task completed!");
+                    double someThingUnused = 0;
+                    //Thread.Sleep(constants.Delay);
+                    for (int j = 0; j < constants.MAX; j++)
+                    {
+                        someThingUnused = someThingUnused + new Random(j).Next(10);
+                    }
+                    return $"Task completed! {someThingUnused}";
                 }, TaskCreationOptions.LongRunning));
+            }
+            while (tasks.Count() > 0)
+            {
+                var index = Task.WaitAny(tasks.ToArray());
+                Console.WriteLine(tasks[index].Result);
+                tasks.RemoveAt(index);
+
             }
         }
     }
